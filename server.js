96ffbea2bc52c1faaebeb79afae161f3bbc69c33
Zahra <bghzahra@outlook.com>
2022-05-27 16:24:10 +0100

    added authorisation for wisdom button

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+process.env.GOOGLE_APPLICATION_CREDENTIALS =
+"secret/firebase-service-account-secrets.json";
+
 const express = require("express");
 const { getAncientWisdom } = require("./bookOfAncientWisdom");
-
+const { initializeApp } = require("firebase-admin/app");
 const cors = require("cors");
+const { getAuth } = require("firebase-admin/auth");
+
 
 const app = express();
 app.use(cors());
-
+const fireBaseApp = initializeApp() 
 const port = process.env.PORT || 4000;
 
 //This route stays public for all
@@ -15,7 +20,14 @@ app.get("/", (req, res) => {
 
 //TODO: Your task will be to secure this route to prevent access by those who are not, at least, logged in.
 app.get("/wisdom", (req, res) => {
-  console.log(req.headers)
+  const authHeaderValue = req.get("Authorization")
+  const [junk, accessToken] = authHeaderValue.split(" ")
+ 
+  getAuth().verifyIdToken(accessToken).then((decodedtoken) => {
+    console.log("hurray, verified token", decodedtoken)
+    res.send("🤐: " + getAncientWisdom() + "🤫");
+  })
+  
   //Eventual plan:
   //1. authHeader = get the value of the Authorization header
   //2. potentialToken = strip the "Bearer " prefix from authHeader
@@ -23,7 +35,6 @@ app.get("/wisdom", (req, res) => {
   //4.     return protected info in response
   //5. else
   //       say access denied in response
-  res.send("🤐: " + getAncientWisdom() + "🤫");
 });
 
 app.listen(port, () => {
